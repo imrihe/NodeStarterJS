50f2d40df8d5b1bab475cec9aa19643ff6a99df4
initshe <initshe@yahoo.com>
2014-11-12 09:12:53 +0000

    move image dir to conf

--- routes/images.js
+++ routes/images.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart({uploadDir: 'uploads/userImages'});
+var multipartMiddleware = multipart({uploadDir: global.conf.imagesDir});
 var imageModel = require('../models/imageModel.js')
 //OCR: https://api.idolondemand.com/1/api/sync/ocrdocument/v1?apikey=7bac1a45-4335-45b8-8920-4044d84e2404&url=IMAGE_URL&mode=document_photo
 
