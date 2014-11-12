f3202a01a96254f2c2c01f90b7d8c91d5844c58d
initshe <initshe@yahoo.com>
2014-11-12 09:32:08 +0000

    step-1

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path =          require('path');
 var logger =        require('morgan');
 var bodyParser =    require('body-parser');
 
-global.conf =       require('./conf.js');
-var routes =        require('./routes/index');
-var images =         require('./routes/images');
 
 var app = express();
 
@@ -13,53 +10,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-app.use(express.static(path.join(__dirname, 'uploads')));  //TODO: add authentication to this static content..
-
-app.use('/', routes);
-app.use('/images', images);
+//TODO: add your content here
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 
 app.set('port', process.env.PORT || 3000);
+
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
 });
