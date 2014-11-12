1ab0bebfb59e6f350065535e3ac59382a893be49
initshe <initshe@yahoo.com>
2014-11-12 13:50:29 +0000

    minimal start

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 //TODO: add your content here
 
