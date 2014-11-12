4cd60b6c19e80d7eb715bcc6d4df8924d70e0785
initshe <initshe@yahoo.com>
2014-11-12 08:23:17 +0000

    step-4-nodb

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
+//TODO: users = userModel  ->you need to cread the userModel.js file and import it to here
 
-//OCR: https://api.idolondemand.com/1/api/sync/ocrdocument/v1?apikey=7bac1a45-4335-45b8-8920-4044d84e2404&url=IMAGE_URL&mode=document_photo
 
 
 
